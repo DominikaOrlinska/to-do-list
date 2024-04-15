8931f6a2c7cc1b7b1f541e461876d606030e7cfe
Dominika Orlinska <dominika.orlinska1@gmail.com>
2024-04-15 13:31:35 +0200

    binding events moved to a separated function

--- js/script.js
+++ js/script.js
@@ -23,6 +23,30 @@
         tasks.splice(taskIndex, 1);
                 render();
     }
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+    }
+    
     const render = () => {
         let htmlString = "";
 
@@ -31,6 +55,7 @@
             <li
             ${task.done ? " style=\"text-decoration: line-through\"" : ""}
             >
+            <button class="js-done">zrobione?</button>
             <button class="js-remove">usuń</button>
             ${task.content}
             </li>
@@ -39,13 +64,7 @@
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
-        const removeButtons = document.querySelectorAll(".js-remove");
-
-        removeButtons.forEach((removeButton, index) => {
-            removeButton.addEventListener("click", () => {
-                removeTask(index);
-            });
-        });
+        bindEvents();
     };
 
     
