c26e9909b669ace276895a75e438f618feeb5b43
Dominika Orlinska <dominika.orlinska1@gmail.com>
2024-07-01 16:23:34 +0200

    added autofocus in new task input

--- js/script.js
+++ js/script.js
@@ -60,14 +60,15 @@
     const onFormSubmit = (event) => {
         event.preventDefault();
 
-        const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
-        if (newTaskContent === "") {
-            return;
+        const newTaskElement = document.querySelector(".js-newTask");
+        const newTaskContent = newTaskElement.value.trim();
+        if (newTaskContent !== "") {
+            addNewTask(newTaskContent);
+            newTaskElement.value = "";
         }
 
-        addNewTask(newTaskContent);
-
+        newTaskElement.focus();
+        
     }
 
     const init = () => {
